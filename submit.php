f01cdb243b2af6aec8000a348215d92286e51d97
Eng-Raphael <eng.rafael362.alfy@gmail.com>
2023-04-12 13:55:28 +0200

    add mod

--- submit.php
+++ submit.php
@@ -2,151 +2,120 @@
 require './PHPMailer/src/PHPMailer.php';
 require './PHPMailer/src/SMTP.php';
 require './PHPMailer/src/Exception.php';
-
 $errors = [];
-
-function test_input($data)
-{
-	$data = trim($data);
-	$data = stripslashes($data);
-	$data = htmlspecialchars($data);
-	return $data;
-}
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	// Get form data
-	if (empty($_POST['firstName'])) {
-		$errors['firstName'] = 'First name is required.';
-	} else {
-		$firstName = test_input($_POST['firstName']);
-		// check if name only contains letters and whitespace
-		if (!preg_match("/^[a-zA-Z ]*$/", $firstName)) {
-			$errors['firstName'] = 'Only letters and white space allowed for first name.';
-		}
+	$firstName = trim($_POST['firstName']);
+	$lastName = trim($_POST['lastName']);
+	$address = trim($_POST['address']);
+	$country = trim($_POST['country']);
+	$gender = trim($_POST['gender']);
+	$username = trim($_POST['username']);
+	$password = trim($_POST['password']);
+	$department = trim($_POST['department']);
+	$skills = isset($_POST['skills']) ? $_POST['skills'] : [];
+
+	// Validate form data
+	if (empty($firstName)) {
+		$errors[] = "First name is required";
+	} else if (!preg_match("/^[a-zA-Z' -]+$/", $firstName)) {
+		$errors[] = "First name should only contain letters, spaces, apostrophes, or hyphens";
 	}
 
-	if (empty($_POST['lastName'])) {
-		$errors['lastName'] = 'Last name is required.';
-	} else {
-		$lastName = test_input($_POST['lastName']);
-		// check if name only contains letters and whitespace
-		if (!preg_match("/^[a-zA-Z ]*$/", $lastName)) {
-			$errors['lastName'] = 'Only letters and white space allowed for last name.';
-		}
+	if (empty($lastName)) {
+		$errors[] = "Last name is required";
+	} else if (!preg_match("/^[a-zA-Z' -]+$/", $lastName)) {
+		$errors[] = "Last name should only contain letters, spaces, apostrophes, or hyphens";
 	}
 
-	if (empty($_POST['address'])) {
-		$errors['address'] = 'Address is required.';
-	} else {
-		$address = test_input($_POST['address']);
+	if (empty($address)) {
+		$errors[] = "Address is required";
 	}
 
-	if (empty($_POST['country'])) {
-		$errors['country'] = 'Country is required.';
-	} else {
-		$country = test_input($_POST['country']);
+	if (empty($country)) {
+		$errors[] = "Country is required";
 	}
 
-	if (empty($_POST['gender'])) {
-		$errors['gender'] = 'Gender is required.';
-	} else {
-		$gender = test_input($_POST['gender']);
+	if (empty($gender)) {
+		$errors[] = "Gender is required";
 	}
 
-	if (empty($_POST['username'])) {
-		$errors['username'] = 'Username is required.';
-	} else {
-		$username = test_input($_POST['username']);
-		// check if username only contains alphanumeric characters
-		if (!preg_match("/^[a-zA-Z0-9]*$/", $username)) {
-			$errors['username'] = 'Only letters and numbers allowed for username.';
-		}
+	if (empty($username)) {
+		$errors[] = "Username is required";
+	} else if (!preg_match("/^[a-zA-Z0-9_\-\.]+$/", $username)) {
+		$errors[] = "Username should only contain letters, numbers, underscores, hyphens, or dots";
 	}
 
-	if (empty($_POST['password'])) {
-		$errors['password'] = 'Password is required.';
-	} else {
-		$password = test_input($_POST['password']);
+	if (empty($password)) {
+		$errors[] = "Password is required";
+	} else if (strlen($password) < 8) {
+		$errors[] = "Password should be at least 8 characters long";
 	}
 
-	if (empty($_POST['department'])) {
-		$errors['department'] = 'Department is required.';
-	} else {
-		$department = test_input($_POST['department']);
+	if (empty($department)) {
+		$errors[] = "Department is required";
 	}
 
-	if (empty($_POST['skills'])) {
-		$errors['skills'] = 'Skills are required.';
-	} else {
-		$skills = test_input($_POST['skills']);
+	if (empty($skills)) {
+		$errors[] = "Skills are required";
 	}
 
-	function test_input($data)
-	{
-		$data = trim($data);
-		$data = stripslashes($data);
-		$data = htmlspecialchars($data);
-		return $data;
-	}
-
-	if (count($errors) > 0) {
-		// display errors on the form
+	if (!empty($errors)) {
 		foreach ($errors as $error) {
-			echo '<p class="error">' . $error . '</p>';
+			echo $error;
 		}
+	}
+
+	// Format email body
+	$message = "Registration Form Submission\n";
+	$message .= "First Name: " . $firstName . "\n";
+	$message .= "Last Name: " . $lastName . "\n";
+	$message .= "Address: " . $address . "\n";
+	$message .= "Country: " . $country . "\n";
+	$message .= "Gender: " . $gender . "\n";
+	$message .= "Username: " . $username . "\n";
+	$message .= "Password: " . $password . "\n";
+	$message .= "Department: " . $department . "\n";
+	$message .= "Skills: " . $skills . "\n";
+
+	// Set SMTP credentials
+	$smtpHost = 'smtp-relay.sendinblue.com';
+	$smtpPort = 587;
+	$smtpUsername = 'raphael277.eng.alfy@gmail.com';
+	$smtpPassword = 'RWbtwnL06KpYCrz9';
+
+	// Create SMTP client
+	$smtp = new \PHPMailer\PHPMailer\SMTP();
+	$smtp->SMTPAuth = true;
+	$smtp->Host = $smtpHost;
+	$smtp->Port = $smtpPort;
+	$smtp->Username = $smtpUsername;
+	$smtp->Password = $smtpPassword;
+	$smtp->SMTPSecure = 'tls';
+
+	// Create email message
+	$mail = new \PHPMailer\PHPMailer\PHPMailer();
+	$mail->setFrom('raphael277.eng.alfy@gmail.com');
+	$mail->addAddress('rafy.assaad@gmail.com');
+	$mail->isHTML(false);
+	$mail->Subject = 'php email send lab iti';
+	$mail->Body = $message;
+
+	// Use SMTP client for sending email
+	$mail->isSMTP();
+	$mail->SMTPAuth = true;
+	$mail->Host = $smtpHost;
+	$mail->Port = $smtpPort;
+	$mail->Username = $smtpUsername;
+	$mail->Password = $smtpPassword;
+	$mail->SMTPSecure = 'tls';
+
+	// Send email
+	if (!$mail->send()) {
+		echo 'Mailer Error: ' . $mail->ErrorInfo;
 	} else {
-
-		// Format email body
-		$message = "Registration Form Submission\n";
-		$message .= "First Name: " . $firstName . "\n";
-		$message .= "Last Name: " . $lastName . "\n";
-		$message .= "Address: " . $address . "\n";
-		$message .= "Country: " . $country . "\n";
-		$message .= "Gender: " . $gender . "\n";
-		$message .= "Username: " . $username . "\n";
-		$message .= "Password: " . $password . "\n";
-		$message .= "Department: " . $department . "\n";
-		$message .= "Skills: " . $skills . "\n";
-
-		// Set SMTP credentials
-		$smtpHost = 'smtp-relay.sendinblue.com';
-		$smtpPort = 587;
-		$smtpUsername = 'raphael277.eng.alfy@gmail.com';
-		$smtpPassword = 'RWbtwnL06KpYCrz9';
-
-		// Create SMTP client
-		$smtp = new \PHPMailer\PHPMailer\SMTP();
-		$smtp->SMTPAuth = true;
-		$smtp->Host = $smtpHost;
-		$smtp->Port = $smtpPort;
-		$smtp->Username = $smtpUsername;
-		$smtp->Password = $smtpPassword;
-		$smtp->SMTPSecure = 'tls';
-
-		// Create email message
-		$mail = new \PHPMailer\PHPMailer\PHPMailer();
-		$mail->setFrom('raphael277.eng.alfy@gmail.com');
-		$mail->addAddress('rafy.assaad@gmail.com');
-		$mail->isHTML(false);
-		$mail->Subject = 'php email send lab iti';
-		$mail->Body = $message;
-
-		// Use SMTP client for sending email
-		$mail->isSMTP();
-		$mail->SMTPAuth = true;
-		$mail->Host = $smtpHost;
-		$mail->Port = $smtpPort;
-		$mail->Username = $smtpUsername;
-		$mail->Password = $smtpPassword;
-		$mail->SMTPSecure = 'tls';
-
-		// Send email
-		if (!$mail->send()) {
-			echo 'Mailer Error: ' . $mail->ErrorInfo;
-		} else {
-			// Redirect to success page
-			header('Location: success.html');
-			exit();
-		}
+		// Redirect to success page
+		header('Location: success.html');
+		exit();
 	}
 }
